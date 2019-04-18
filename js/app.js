e60a3b8c030bd9ac2436cd068ff2ce9da151d18d
Paweł Antosiak <pawel.assasz678@gmail.com>
2019-04-18 22:06:06 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,5 +1,8 @@
 $(document).ready(function () {
-    $('.intro').bgVideo();
+    $('.intro').bgVideo({
+        fadeOnPause: true,
+        showPausePlay: true
+    });
 
     $('.intro').mouseover(function () {
         this.play();
