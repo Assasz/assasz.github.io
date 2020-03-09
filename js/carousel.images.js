a94e74ef3f9467ba9f294445e4648927f998248b
manipurafiredance <54290516+manipurafiredance@users.noreply.github.com>
2020-03-09 01:37:31 +0100

    Update carousel.images.js

--- js/carousel.images.js
+++ js/carousel.images.js
@@ -1,5 +1,20 @@
 const carouselImages = [
     {
+        src: "images/gallery/17.jpg",
+        thumbSrc: "images/gallery/17min.jpg",
+        desc: "Wedding show"
+    },
+    {
+        src: "images/gallery/16.jpg",
+        thumbSrc: "images/gallery/16min.jpg",
+        desc: "Wedding show"
+    },
+    {
+        src: "images/gallery/5.jpg",
+        thumbSrc: "images/gallery/min5.jpg",
+        desc: "Obrazy ogniem malowane 2017"
+    },    
+    {
         src: "images/gallery/1.jpg",
         thumbSrc: "images/gallery/min1.jpg",
         desc: "Ostróda festiwal ognia"
@@ -20,24 +35,9 @@ const carouselImages = [
         desc: "Grupa FireShow"
     },
     {
-        src: "images/gallery/17.jpg",
-        thumbSrc: "images/gallery/17min.jpg",
-        desc: "Wedding show"
-    },
-    {
-        src: "images/gallery/4.jpg",
-        thumbSrc: "images/gallery/min4.jpg",
-        desc: "Protokultura, Psycho BBQ"
-    },
-    {
         src: "images/gallery/10.jpg",
         thumbSrc: "images/gallery/min10.jpg",
         desc: "Jarmark Bożonarodzeniowy Gdańsk 2018"
-    },
-    {
-        src: "images/gallery/5.jpg",
-        thumbSrc: "images/gallery/min5.jpg",
-        desc: "Obrazy ogniem malowane 2017"
     },
     {
         src: "images/gallery/6.jpg",
@@ -45,23 +45,10 @@ const carouselImages = [
         desc: "Obrazy ogniem malowane 2017"
     },
     {
-        src: "images/gallery/8.jpg",
-        thumbSrc: "images/gallery/8.jpg",
-        desc: "Sabat Czarownic 2018, Protokultura"
-    },
-    {
         src: "images/gallery/12.jpg",
         thumbSrc: "images/gallery/12min.jpg",
         desc: "Led session: Mateusz Koszykowski, Aga Koncewicz, Paweł Jakubiszyn"
     },
-    {
-        src: "images/gallery/13.jpg",
-        thumbSrc: "images/gallery/13min.jpg",
-        desc: "Led session: Mateusz Koszykowski, Paweł Jakubiszyn"
-    },
-    {
-        src: "images/gallery/16.jpg",
-        thumbSrc: "images/gallery/16min.jpg",
-        desc: "Wedding show"
-    },    
+
+    
 ]
